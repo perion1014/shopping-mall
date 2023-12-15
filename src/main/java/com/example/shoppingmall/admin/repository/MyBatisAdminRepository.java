0d926b05ecf49a7cf0f75804f9d87dc16ed5c692
TITANIUM <bslee351@gmail.com>
2023-12-15 14:40:41 +0900

    lbs push

---
+++ src/main/java/com/example/shoppingmall/admin/repository/MyBatisAdminRepository.java
@@ -0,0 +1,16 @@
+package com.example.shoppingmall.admin.repository;
+
+import com.example.shoppingmall.admin.domain.Admin;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public class MyBatisAdminRepository implements AdminRepository{
+
+    @Override
+    public void adminLogin(Admin admin){
+
+    }
+}
