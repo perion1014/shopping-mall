1f94ec7d2f3092d49a27b4a4e1bd5f8126e26339
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-12-18 12:33:42 +0900

    commit at 12:33

---
+++ src/main/java/com/example/shoppingmall/cart/dto/CartReadDTO.java
@@ -0,0 +1,35 @@
+package com.example.shoppingmall.cart.dto;
+
+import com.example.shoppingmall.cart.domain.Cart;
+import com.example.shoppingmall.cart.service.CartService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartReadDTO {
+    private String itemThumbnail;
+    private String itemName;
+    private String itemSize;
+    private Integer itemPrice;
+    private Integer itemQuantity;
+    private Integer itemPriceSum;
+    private Integer orderPriceSum;
+
+    public static CartReadDTO CartToCartDTO(Cart cart){
+    CartReadDTO cartReadDTO = new CartReadDTO();
+    //cartReadDTO.setItemThumbnail(CartService.getItemThumbnail()); // item_photos에서 가져옴
+    cartReadDTO.setItemName(CartService.getItemName());           // item에서 가져옴
+    cartReadDTO.setItemSize(CartService.getItemSize());           // cart에서 가져옴
+    cartReadDTO.setItemPrice(CartService.getItemPrice());         // item에서 가져옴
+    cartReadDTO.setItemQuantity(CartService.getItemQuantity());   // cart에서 가져옴
+    cartReadDTO.setItemPriceSum(CartService.getItemPriceSum());   // itemPrice로 계산
+    cartReadDTO.setOrderPriceSum(CartService.getOrderPriceSum()); // otemPriceSum으로 계산
+
+    return cartReadDTO;
+    }
+
+}
