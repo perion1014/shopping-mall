5383a31a504a2c0a6c1e61f8a4edc3005acaac48
TITANIUM <bslee351@gmail.com>
2023-12-18 12:32:59 +0900

    lbs commit

--- src/main/java/com/example/shoppingmall/admin/repository/AdminRepository.java
+++ src/main/java/com/example/shoppingmall/admin/repository/AdminRepository.java
@@ -1,6 +1,9 @@
 package com.example.shoppingmall.admin.repository;
 
 import com.example.shoppingmall.admin.domain.Admin;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
--- src/main/java/com/example/shoppingmall/admin/service/AdminService.java
+++ src/main/java/com/example/shoppingmall/admin/service/AdminService.java
@@ -1,6 +1,8 @@
 package com.example.shoppingmall.admin.service;
 
 import com.example.shoppingmall.admin.domain.Admin;
+import com.example.shoppingmall.admin.dto.AdminLoginDTO;
+import com.example.shoppingmall.admin.mapper.AdminMapper;
 import com.example.shoppingmall.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,12 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-    private  final AdminRepository adminRepository;
-    @Transactional//
-    public void join(Admin admin){
-        adminRepository.adminLogin(admin);
 
+    private final AdminMapper adminMapper;
 
-    }
+//    public String adminAutority(String adminId){
+//        String resultId = adminMapper.adminLogin(adminId);
+//    return resultId;
+//    }
 }
-
