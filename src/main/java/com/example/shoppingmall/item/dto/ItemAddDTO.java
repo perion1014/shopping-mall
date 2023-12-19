b045c0fb88dd8977ccea100ea115b91592f7178b
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2023-12-19 16:05:28 +0900

    the bufs in itemmapper.xml fixed

--- src/main/java/com/example/shoppingmall/item/dto/ItemAddDTO.java
+++ src/main/java/com/example/shoppingmall/item/dto/ItemAddDTO.java
@@ -16,10 +16,11 @@ public class ItemAddDTO {
     private Long itemNo;
     private String itemName;
     private String itemCategory;
-    private Double itemGrade;
-    private String itemDetail;
     private Integer itemPrice;
     private Integer itemOnsale;
+    private String itemSize;
+    private Integer itemStock;
+    private String itemDetail;
     private Timestamp itemRegisteredTime;
 
     public static Item itemAddDTOToItem(ItemAddDTO itemAddDTO) {
@@ -27,10 +28,9 @@ public class ItemAddDTO {
         item.setItemNo(itemAddDTO.getItemNo());
         item.setItemName(itemAddDTO.getItemName());
         item.setItemCategory(itemAddDTO.getItemCategory());
-        item.setItemGrade(itemAddDTO.getItemGrade());
-        item.setItemDetail(itemAddDTO.getItemDetail());
         item.setItemPrice(itemAddDTO.getItemPrice());
         item.setItemOnsale(itemAddDTO.getItemOnsale());
+        item.setItemDetail(itemAddDTO.getItemDetail());
         item.setItemRegisteredTime(itemAddDTO.getItemRegisteredTime());
         return item;
     }
@@ -39,10 +39,9 @@ public class ItemAddDTO {
         itemAddDTO.setItemNo(item.getItemNo());
         itemAddDTO.setItemName(item.getItemName());
         itemAddDTO.setItemCategory(item.getItemCategory());
-        itemAddDTO.setItemGrade(item.getItemGrade());
-        itemAddDTO.setItemDetail(item.getItemDetail());
         itemAddDTO.setItemPrice(item.getItemPrice());
         itemAddDTO.setItemPrice((item.getItemOnsale()));
+        itemAddDTO.setItemDetail(item.getItemDetail());
         itemAddDTO.setItemRegisteredTime(item.getItemRegisteredTime());
         return itemAddDTO;
     }
