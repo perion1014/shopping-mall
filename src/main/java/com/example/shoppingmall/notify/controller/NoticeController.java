b079a83c69b7bc71a171490a93b8ca6aa22530c9
TITANIUM <bslee351@gmail.com>
2023-12-20 22:16:22 +0900

    notice CRud

--- src/main/java/com/example/shoppingmall/notify/controller/NoticeController.java
+++ src/main/java/com/example/shoppingmall/notify/controller/NoticeController.java
@@ -4,7 +4,10 @@ import com.example.shoppingmall.notify.dto.NoticeAddDTO;
 import com.example.shoppingmall.notify.dto.NoticeDeleteDTO;
 import com.example.shoppingmall.notify.dto.NoticeListDTO;
 import com.example.shoppingmall.notify.service.NoticeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,34 +27,35 @@ public class NoticeController {
         model.addAttribute("noticeListDTOList", noticeService.findAllNotice());
         return "admins/notice/admins-notice";
     }
-    /*공지사항 디테일*/
+    /*공지사항 디테일 Admin전용*/
     @GetMapping("/admin/{noticeNo}")
-    public String showNoticeDetail(@PathVariable(name="noticeNo")Integer noticeNo, Model model){
+    public String showNoticeDetail(@PathVariable(name="noticeNo")Long noticeNo, Model model){
         return "admins/notice/admins-notice-detail";
     }
-    /*공지사항 작성페이지 이동*/
+    /*공지사항 작성페이지 이동 Admin전용*/
     @GetMapping("/admin/add")
     public String goToRegisterNoticePage(){
         return "admins/notice/admins-notice-add";
     }
-    /*공지사항 작성실행*/
-
+    /*공지사항 작성실행 Admin전용*/
     @PostMapping("/admin/add")
-    public String registerNotice(@RequestParam("adminNo") Long adminNo, @ModelAttribute NoticeAddDTO noticeAddDTO){
+    public String registerNotice(@RequestParam("adminNo") Integer adminNo, @ModelAttribute NoticeAddDTO noticeAddDTO){
+        //adminId도 가져와야함 @RequestParam("adminId") String adminId
+        System.out.println("adminNo = " + adminNo);
         noticeService.addNotice(adminNo, noticeAddDTO);
         return "redirect:/notice/admin";
     }
-    /*공지사항 수정페이지*/
+    /*공지사항 수정페이지 Admin전용*/
     @GetMapping("/admin/{noticeNo}/update")
     public String goToUpdateNoticePage(@PathVariable(name="noticeNo")Long noticeNo, Model model){
         return "admins/notice/admins-notice-modify";
     }
-    /*공지사항 수정실행*/
+    /*공지사항 수정실행 Admin전용*/
     @PostMapping("/admin/{noticeNo}/update")
     public String updateNotice(@PathVariable(name= "noticeNo") Long noticeNo, Model model){
         return "admins/notice/admins-notice-modify";
     }
-    /*공지사항 삭제*/
+    /*공지사항 삭제 Admin전용*/
     @PostMapping("/admin/{noticeNo}/delete")
     public String deleteNotice(@PathVariable(name= "noticeNo") Long noticeNo, NoticeDeleteDTO noticeDeleteDTO, Model model){
 
