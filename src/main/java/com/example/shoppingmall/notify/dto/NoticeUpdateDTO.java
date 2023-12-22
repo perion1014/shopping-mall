514eb0437f4600bfa584e2203ed1e6ed51621092
TITANIUM <bslee351@gmail.com>
2023-12-22 16:17:47 +0900

    notice modify

--- src/main/java/com/example/shoppingmall/notify/dto/NoticeUpdateDTO.java
+++ src/main/java/com/example/shoppingmall/notify/dto/NoticeUpdateDTO.java
@@ -11,17 +11,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NoticeUpdateDTO {
-
+    private Long noticeNo;
     private String noticeTitle;
     private String noticeContent;
+    //private Timestamp noticeCreatedTime;수정시간이 notice-modify.html로 전달해야함
+    private Long noticeViewcount;
 
     public static Notice noticeUpdateDTOToNotice(Long noticeNo,NoticeUpdateDTO noticeUpdateDTO){
         Notice notice = new Notice();
         notice.setNoticeNo(notice.getNoticeNo());
         notice.setNoticeTitle(noticeUpdateDTO.getNoticeTitle());
         notice.setNoticeContent(noticeUpdateDTO.getNoticeContent());
+        notice.setNoticeViewcount(noticeUpdateDTO.getNoticeViewcount());
         return notice;
 
     }
+    public static NoticeUpdateDTO NoticeToNoticeUpdateDTO(Notice notice){
+        NoticeUpdateDTO noticeUpdateDTO = new NoticeUpdateDTO();
+        noticeUpdateDTO.setNoticeNo(notice.getNoticeNo());
+        noticeUpdateDTO.setNoticeTitle(notice.getNoticeTitle());
+        noticeUpdateDTO.setNoticeContent(notice.getNoticeContent());
+        //수정시간이 들어가야됨
+        noticeUpdateDTO.setNoticeViewcount(notice.getNoticeViewcount());
+        return noticeUpdateDTO;
+    }
+
 
 }
