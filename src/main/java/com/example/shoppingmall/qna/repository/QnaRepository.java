118779f17517f5c055a41017840983f8c9e45ea1
swaeluu <d214b@naver.com>
2023-12-27 14:36:44 +0900

    feat: (user) add QnA

--- src/main/java/com/example/shoppingmall/qna/repository/QnaRepository.java
+++ src/main/java/com/example/shoppingmall/qna/repository/QnaRepository.java
@@ -4,9 +4,12 @@ import com.example.shoppingmall.qna.domain.Qna;
 
 import java.util.List;
 
+
 public interface QnaRepository {
 
     List<Qna> findByItemNo(Long itemNo);
 
     String getMemberIdByNo(Long memberNo);
+
+    void addQna(Qna qna);
 }
