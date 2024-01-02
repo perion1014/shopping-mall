457045afe8cbeaf9b7b3949531d12d2d5a1d2103
Ainchel <134568940+Ainchel@users.noreply.github.com>
2024-01-02 14:42:30 +0900

    feat: making nonMember-Order-ItemStockCheck 2ndCommit

--- src/main/java/com/example/shoppingmall/order/controller/OrderController.java
+++ src/main/java/com/example/shoppingmall/order/controller/OrderController.java
@@ -30,9 +30,9 @@ public class OrderController {
         System.out.println("컨트롤러에 도착 확인");
 
         for(int i =0; i <jsonData.size(); i++){
-
             System.out.println(jsonData.get(i).getItemName());
             System.out.println(jsonData.get(i).getItemSize());
+            System.out.println(jsonData.get(i).getSelectedItemQuantity());
         }
 
         Map<String, Object> responseData = new HashMap<>();
@@ -45,9 +45,13 @@ public class OrderController {
     @ResponseBody
     public Map<String, Object> checkNonMemberOrderItemStock(@RequestBody List<MemberOrderItemStockCheckDTO> jsonData){
 
+        System.out.println("컨트롤러 도착 확인");
 
-
-
+        for(int i = 0; i < jsonData.size(); i++){
+            System.out.println(jsonData.get(i).getItemName());
+            System.out.println(jsonData.get(i).getItemSize());
+            System.out.println(jsonData.get(i).getSelectedItemQuantity());
+        }
 
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("response", "데이터 전달 후 응답 확인");
