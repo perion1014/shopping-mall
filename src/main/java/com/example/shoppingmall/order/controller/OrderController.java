e74ea60d291bafc6f907b1c64e9e91080d8c5b89
HyonHyon <khm3626@gmail.com>
2024-01-07 12:27:30 +0900

    fix: non-memeber-order bug

--- src/main/java/com/example/shoppingmall/order/controller/OrderController.java
+++ src/main/java/com/example/shoppingmall/order/controller/OrderController.java
@@ -109,7 +109,9 @@ public class OrderController {
 
 
     @GetMapping("/orders/create")
-    public String goToInputNonMemberOrderPage(HttpServletRequest req){
+    public String goToInputNonMemberOrderPage(
+            @ModelAttribute("nonMemberOrderAddDTO") NonMemberOrderAddDTO nonMemberOrderAddDTO,
+            HttpServletRequest req){
 
 //        HttpSession session = req.getSession();
 //        List<NonMemberOrderDetailAddDTO> nonMemberOrderDetailAddDTOList = (List<NonMemberOrderDetailAddDTO>) session.getAttribute("nonMemberOrderDetailAddDTOList");
@@ -128,7 +130,8 @@ public class OrderController {
 
     /*PRG 패턴 반드시 써주세요 */
     @PostMapping("/orders/create")
-    public String makeNonMemberOrder(@Validated(OrderValidationSequence.class) @ModelAttribute("nonMemberOrderAddDTO") NonMemberOrderAddDTO nonMemberOrderAddDTO,
+    public String makeNonMemberOrder(@Validated(OrderValidationSequence.class)
+            @ModelAttribute("nonMemberOrderAddDTO") NonMemberOrderAddDTO nonMemberOrderAddDTO,
                                      BindingResult bindingResult,
                                      HttpServletRequest req){
 
